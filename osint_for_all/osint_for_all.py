f0dedc0213ff5a1b6959d0eb5f0fcd476cf280f8
Davin Hong <davin.hong3@gmail.com>
2024-09-13 06:11:26 +0000

    V1 - Virustotal module for IP and URL/domain & Urlscan
    To clean up urlscan output
    Add file hash report handler for VT submission

---
+++ osint_for_all/osint_for_all.py
@@ -0,0 +1,189 @@
+import requests
+import argparse
+import sys
+from tabulate import tabulate
+import ipaddress
+import json
+import whois
+from urlscan import *
+
+"""
+Current integrations include:
+1. VirusTotal 
+2. Urlscan.io
+3. siterview (to overcome script/scrapper blockers) 
+3. whois (to fix, servers perpetually time out)
+"""
+
+#  pip install python-whois
+
+urlscan_api_key = 'InsertYourAPIKey' # 150k public scans per day
+API_KEY = 'InsertYourAPIKey'  # Replace with your VirusTotal API key
+# 4 lookups per min, 500 per day (15.5k per month)
+
+# // COLORS
+RED = "\033[91m"
+YELLOW = "\033[93m"
+LIGHT_GREEN = "\033[92;1m"
+LIGHT_BLUE = "\033[96m"
+RESET = "\033[0m"
+
+# // VirusTotal Module 
+def get_report(resource):
+    url = 'https://www.virustotal.com/vtapi/v2/url/report'
+    params = {'apikey': API_KEY, 'resource': resource}
+    response = requests.get(url, params=params)
+    return response.json()
+
+def scan_url(resource):
+    url = 'https://www.virustotal.com/vtapi/v2/url/scan'
+    params = {'apikey': API_KEY, 'url': resource}
+    response = requests.post(url, data=params)
+    return response.json()
+
+def scan_ip(resource):
+    url = f'https://www.virustotal.com/api/v3/ip_addresses/{resource}'
+
+    headers = {
+        'Accept': 'application/json',
+        'x-apikey': API_KEY
+    }
+
+    response = requests.request(method='GET', url=url, headers=headers)
+    decodedResponse = json.loads(response.text)
+    return decodedResponse   
+
+# Parse output from VT for IP analysis
+def filter_data(data): 
+    # Add: network, last_analysis_stats (malicious, suspicious),
+    if 'error' in data:
+        details = f"Details: {data['error']['message']}"
+        output = f"\n[-] VirusTotal\n{details}\n"
+    elif 'private' in data['data']['attributes']['tags']:
+        ip_address = data['data']['id']
+        output = f"\n[-] VirusTotal\nIP Address {ip_address} is not a public IP Address\n"
+    else:
+        if 'network' in data['data']['attributes']:
+            network = f"Network: {data['data']['attributes']['network']}"
+        else:
+            network = "Network: N/A"
+        harmless_report = f" - Harmless: {data['data']['attributes']['last_analysis_stats']['harmless']}"
+        malicious_report = f" - Malicious: {data['data']['attributes']['last_analysis_stats']['malicious']}"
+        suspicious_report = f" - Suspicious: {data['data']['attributes']['last_analysis_stats']['suspicious']}"
+        country = f"Country: {data['data']['attributes']['country']}"
+        AS_no = f"AS Number: {data['data']['attributes']['whois']}"
+
+        link = f"{LIGHT_GREEN}[*] VirusTotal results link:{RESET}\n{data['data']['links']['self']}"
+        output = f"\n[*] VirusTotal\n{LIGHT_GREEN}Security Vendors' Analysis{RESET}\n{harmless_report}\n{malicious_report}\n{suspicious_report}\n{network}\n\n{LIGHT_GREEN}[*] Geolocation & Other information{RESET}\n{country}\n{AS_no}\n{link}"
+
+    return output
+
+# function to list keys by indent
+def format_report(report):
+    if 'scans' in report:
+        table_data = []
+        for scanner, result in report['scans'].items():
+            table_data.append([scanner, result['detected'], result['result']])
+        return tabulate(table_data, headers=['Scanner', 'Detected', 'Result'], tablefmt='grid')
+    else:
+        return '[!] No scan data available.'
+
+# select certain keys in the json output to output
+def print_selected_keys(json_obj, keys):
+    """
+    Prints the specified keys and their values from a JSON object.
+    
+    :param json_obj: The JSON object (dictionary) to print keys from.
+    :param keys: A list of keys to extract and print.
+    """
+    total = json_obj['total']
+
+    for key in keys:
+        if key in json_obj:
+            if key == 'positives':
+                print(f'Number of hits on VT: {json_obj[key]}/{total}')
+            elif key == 'total':
+                pass
+            else:
+                print(f'{key}: {json_obj[key]}')
+           
+# Define keys we need from VT output 
+keys_to_extract = ['permalink', 'positives', 'resource', 'scan_date', 'scan_id', 'total', 'url']
+
+
+# checks if input is ip address
+def is_ip_address(input_str):
+    """
+    Checks if the input string is a valid IP address.
+    
+    :param input_str: The input string to check.
+    :return: True if the input is a valid IP address, False otherwise.
+    """
+    try:
+        ipaddress.ip_address(input_str)
+        return True
+    except ValueError:
+        return False
+    
+# // Whois Module
+def whois_scan(domain):
+    try:
+        whois_response = whois.whois(domain)
+        return whois_response
+    except Exception as exception:
+        return f"[!] Error occured while fetching Whois response: {exception}"
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Scan an IP address, URL, or domain using VirusTotal API.')
+    parser.add_argument('resource', help='The IP address, URL, or domain to scan.')
+    args = parser.parse_args()
+
+    resource = args.resource
+
+    if is_ip_address(resource):
+        """
+        Perform IP Address OSINT Exposure checks: VT, abuseipdb, DNS resolution
+        """
+        print(f'{LIGHT_GREEN}[*] Submitting the IP address indicator on VirusTotal: {resource}{RESET}')
+    
+        scan_result = scan_ip(resource)
+        #print(scan_result) # submission URL
+        
+        print('[*] Scan request successfully submitted.')
+        report = filter_data(scan_result)
+        print(f'\n{YELLOW}=============================== VirusTotal ==============================={RESET}')                
+        print(report)
+        
+
+    else: # Non-IP address: URL, domain, File Hash
+        """
+        Perform VT, Sitereview, URLscan, Whois
+        """
+        print(f'{LIGHT_GREEN}[*] Submitting the indicator on VirusTotal: {resource}{RESET}')
+    
+        scan_result = scan_url(resource)
+        #print(scan_result) # submission URL
+        if scan_result['response_code'] == 1:
+            print('[*] Scan request successfully submitted.')
+            report = get_report(resource)
+            #print(report) # response
+            
+            if report['response_code'] == 1:
+                print(f'\n{YELLOW}=============================== VirusTotal ==============================={RESET}')
+                # Select certain keys and print
+                print_selected_keys(report, keys_to_extract)
+            else:
+                print('[!] Error fetching report:', report['verbose_msg'])
+        else:
+            print('[!] Error submitting scan request:', scan_result['verbose_msg'])
+
+
+        print(f'[*] Submitting the indicator on Urlscan: {resource}')
+        print('\n=============================== Urlscan ===============================')
+        urlscan_submit_retrieve(resource, urlscan_api_key)
+        
+
+
+if __name__ == '__main__':
+    main()
---
+++ osint_for_all/urlscan.py
@@ -0,0 +1,69 @@
+import requests
+import time
+import json
+ 
+# API key needed
+# Query own API key current limit using this: 
+# https://urlscan.io/docs/api/
+
+def submit_to_urlscan(url, api_key):
+    headers = {'API-Key': api_key, 'Content-Type': 'application/json'}
+    data = {"url": url, "visibility": "public"}
+    response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, json=data)
+    if response.status_code == 200:
+        # The response to the API call will return the following JSON object, including the unique scan UUID and API endpoint for the scan. With the unique UUID you can retrieve the scan result, screenshot and DOM snapshot after waiting for a while. While the scan is still in progress, the Result API endpoint will respond with the 404 code.
+        # The suggested polling logic would be to wait 10-30 seconds directly after submission and then polling in 5-second intervals until the scan is finished or a maximum wait time has been reached.
+        # {
+        #   "message": "Submission successful",
+        #   "uuid": "0e37e828-a9d9-45c0-ac50-1ca579b86c72",
+        #   "result": "https://urlscan.io/result/0e37e828-a9d9-45c0-ac50-1ca579b86c72/",
+        #   "api": "https://urlscan.io/api/v1/result/0e37e828-a9d9-45c0-ac50-1ca579b86c72/",
+        #   "visibility": "public",
+        #   "options": {
+        #     "useragent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+        #   },
+        #   "url": "https://urlscan.io",
+        #   "country": "de"
+        # }
+        """
+        Solution is to split into submission & report retrieval functions
+        """
+
+        scan_uuid = response.json().get('uuid')
+        return scan_uuid
+    else:
+        print(f"[URLSCAN] RESPONSE={response.status_code}")
+        return None
+
+# Default delay is 15s for the scan results, then use scan ID from submission API to poll for scan
+def get_urlscan_result(scan_uuid, api_key, retries=4, delay=15):
+    result_url = f'https://urlscan.io/api/v1/result/{scan_uuid}/'
+    headers = {'API-Key': api_key}
+
+    for attempt in range(retries):
+        time.sleep(delay)  # Wait before checking if the scan is ready
+        response = requests.get(result_url, headers=headers)
+        if response.status_code == 200:
+            scan_data = response.json()
+            return scan_data
+        else:
+            print(f"[URLSCAN] ATTEMPT={attempt + 1}/{retries}, Reasons: 404-scan still ongoing, 410-scan result has been deleted, Please check status code:\n[*] Status Code={response.status_code}")
+
+    return None
+
+
+def urlscan_submit_retrieve(url, api_key):
+    scan_uuid = submit_to_urlscan(url, api_key)
+    if scan_uuid:
+        print("Scan submitted successfully.")
+        print("Waiting for scan results...")
+        scan_data = get_urlscan_result(scan_uuid, api_key)
+        if scan_data:
+            print(scan_data)
+            print(f"[*] You can obtain the target URL screenshot here:\nhttps://urlscan.io/screenshots/{scan_uuid}.png")
+            print(f"[*] You can obtain the target DOM snapshot here:\n https://urlscan.io/dom/{scan_uuid}/")
+            # return scan_data
+        else:
+            return "[!] Failed to retrieve scan results."
+    else:
+        return "[!] Failed to submit the URL for scanning."
